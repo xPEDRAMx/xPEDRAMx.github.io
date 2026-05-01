baa2b68a17e4c82b4954329fb22200a09514cc32
Pedram <beigi.pedram@gmail.com>
2026-05-01 13:25:29 -0400

    inor

--- resume/export-pdf.sh
+++ resume/export-pdf.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+OUTPUT_PDF="/Users/pedrambeigi/xPEDRAMx.github.io/resume/resume.pdf"
+
 "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" \
   --headless=new \
   --disable-gpu \
   --allow-file-access-from-files \
-  --print-to-pdf="/Users/pedrambeigi/xPEDRAMx.github.io/resume/resume.pdf" \
+  --no-pdf-header-footer \
+  --print-to-pdf="${OUTPUT_PDF}" \
   "file:///Users/pedrambeigi/xPEDRAMx.github.io/resume/index.html"
